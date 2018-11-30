ae50225267da63884ce874eeb9322b352811f0c0
Sharghi, Shahyad <shahyads@yahoo.com>
2018-11-30 15:42:05 -0500

    StartStopService is added

---
+++ StartStopService/StartStopService.ps1
@@ -0,0 +1,58 @@
+[CmdletBinding()]
+param(
+    [ValidateSet("Start","Stop")][String] $Action,
+    [Parameter(Mandatory=$true)][String] $ServiceName,
+    [Parameter(Mandatory=$true)][string] $UserName,
+    [Parameter(Mandatory=$true)][string] $Password,
+    [Parameter(Mandatory=$true)][string] $MachineNameslist
+)
+
+	# Display vars
+	Write-Host "ASSIGNED PARAMETER VALUES:"
+	Write-Host "Action: $($Action)"
+	Write-Host "ServiceName: $($ServiceName)"
+	Write-Host "UserName: $($UserName)"
+	Write-Host "Password: [masked]"
+	Write-Host "MachineNameslist: $($MachineNameslist)"
+
+   # Stop the script on error
+	$ErrorActionPreference = "Stop"
+$scriptBlock={
+    param($Action,$ServiceName)
+
+    $service = Get-Service | Where-Object {$_.Name -eq $ServiceName} | Select-Object -First 1
+
+    if ($service -ne $null)
+    {
+        switch ($Action){
+            "Start"   
+			{
+				if ($service.status -ne "Running") 
+					Start-Service -Name $ServiceName
+				else
+					write-host "the service is already Running"
+			}
+            "Stop"    
+			{
+				if($service.status -ne "Stopped") 
+					Stop-Service -Name $ServiceName
+				else
+					write-host "the service is already Stopped"
+			}
+        }
+    }    
+    else
+    {
+        Write-Error "The Service '$ServiceName' is not found"
+    }
+}
+
+$securePassword = "$Password" | ConvertTo-SecureString -AsPlainText -Force
+$authCredentials = New-Object System.Management.Automation.PSCredential($UserName, $securePassword)
+
+$MachineNameslist  = $MachineNameslist  -replace " ",""
+$Machines = $MachineNameslist  -split ";"
+foreach($machine in $Machines) 
+{
+	Invoke-Command -ComputerName $machine -Credential $authCredentials -ScriptBlock $scriptBlock -ArgumentList $Action, $ServiceName
+}
